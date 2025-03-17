ff9140db03fc01064dfd35ee955f15a05bf4508c
Radu-Teodor Prosie <psn.radu@gmail.com>
2025-03-17 17:30:42 +0200

    Update main.cpp
    Am facut formatul si codul mai clar

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@ int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr); std::cout.tie(nullptr);
 
-///voi adauga cod cu github actions la fiecare commit
-
   int n; std::vector<int> v, cop;
   for (int i = 0; i <= 10; i++) {
 
@@ -24,9 +22,23 @@ int main() {
     for(int j = 0; j < n; j++)
       std::cin >> v[j];
 
-    cop = v; auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
     auto end = start; auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
+    /*FORMAT:
+
+    cop = v;
+    start = std::chrono::high_resolution_clock::now();
+    std::cout << "NUME_SORTARE: "; NUME_SORTARE(cop);
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << elapsed.count() << " milliseconds\n";
+    assert(is_sorted(cop.begin(), cop.end()));
+
+    */
+    
+    
+    cop = v;
     start = std::chrono::high_resolution_clock::now();
     std::cout << "radixsort: "; radixsort(cop);
     end = std::chrono::high_resolution_clock::now();
