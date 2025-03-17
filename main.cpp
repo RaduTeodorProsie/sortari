76630787304a2f129a5156670ae92fcfa7a47d1a
Radu-Teodor Prosie <psn.radu@gmail.com>
2025-03-17 18:13:14 +0200

    Update main.cpp according to clang

--- main.cpp
+++ main.cpp
@@ -7,54 +7,53 @@
 const std::string path = "../small_tests/";
 
 int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr); std::cout.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr); std::cout.tie(nullptr);
 
-  int n; std::vector<int> v, cop;
-  for (int i = 0; i <= 10; i++) {
+    int n; std::vector<int> v, cop;
+    for (int i = 0; i <= 10; i++) {
 
-    const std::string to_open = path + std::to_string(i) + "-sortare.in";
-    freopen(to_open.c_str(), "r", stdin);
+        const std::string to_open = path + std::to_string(i) + "-sortare.in";
+        freopen(to_open.c_str(), "r", stdin);
 
-    std::cout << "TESTUL " << std::to_string(i) << "\n";
-    std::cin >> n; v.resize(n);
+        std::cout << "TESTUL " << std::to_string(i) << "\n";
+        std::cin >> n; v.resize(n);
 
-    for(int j = 0; j < n; j++)
-      std::cin >> v[j];
+        for(int j = 0; j < n; j++)
+            std::cin >> v[j];
 
-    auto start = std::chrono::high_resolution_clock::now();
-    auto end = start; auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        auto start = std::chrono::high_resolution_clock::now();
+        auto end = start; auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-    /*FORMAT:
+        /*FORMAT:
 
-    cop = v;
-    start = std::chrono::high_resolution_clock::now();
-    std::cout << "NUME_SORTARE: "; NUME_SORTARE(cop);
-    end = std::chrono::high_resolution_clock::now();
-    elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << elapsed.count() << " milliseconds\n";
-    assert(is_sorted(cop.begin(), cop.end()));
+        cop = v;
+        start = std::chrono::high_resolution_clock::now();
+        std::cout << "NUME_SORTARE: "; NUME_SORTARE(cop);
+        end = std::chrono::high_resolution_clock::now();
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << elapsed.count() << " milliseconds\n";
+        assert(is_sorted(cop.begin(), cop.end()));
 
-    */
-    
-    
-    cop = v;
-    start = std::chrono::high_resolution_clock::now();
-    std::cout << "radixsort: "; radixsort(cop);
-    end = std::chrono::high_resolution_clock::now();
-    elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << elapsed.count() << " milliseconds\n";
-    assert(is_sorted(cop.begin(), cop.end()));
+        */
+        
+        cop = v;
+        start = std::chrono::high_resolution_clock::now();
+        std::cout << "radixsort: "; radixsort(cop);
+        end = std::chrono::high_resolution_clock::now();
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << elapsed.count() << " milliseconds\n";
+        assert(is_sorted(cop.begin(), cop.end()));
+        
+        cop = v;
+        start = std::chrono::high_resolution_clock::now();
+        std::cout << "stl_sort: "; stl_sort(cop);
+        end = std::chrono::high_resolution_clock::now();
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << elapsed.count() << " milliseconds\n";
+        assert(is_sorted(cop.begin(), cop.end()));
 
-    cop = v;
-    start = std::chrono::high_resolution_clock::now();
-    std::cout << "stl_sort: "; stl_sort(cop);
-    end = std::chrono::high_resolution_clock::now();
-    elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << elapsed.count() << " milliseconds\n";
-    assert(is_sorted(cop.begin(), cop.end()));
+        std::cout << '\n';
 
-    std::cout << '\n';
-
-  }
+    }
 }
