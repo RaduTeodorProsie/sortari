47450d0b7e9f92db91acd7e89694258ff3394a56
Turcu Andrei Cristian <andreiturcu2005.24@gmail.com>
2025-04-05 17:55:03 +0300

    file for merge..

--- sorts/sortari.h
+++ sorts/sortari.h
@@ -13,29 +13,4 @@ void heapsort(std::vector<int> &v);
 
 void timsort(std::vector<int> &v);
 
-void merge(std::vector<int> &v, int l, int mid, int r) {
-    std::vector<int> aux;
-    int i = l, j = mid + 1;
-  
-    while (i <= mid and j <= r) {
-      if (v[i] < v[j]) {
-        aux.push_back(v[i]);
-        i++;
-      } else {
-        aux.push_back(v[j]);
-        j++;
-      }
-    }
-    while (i <= mid) {
-      aux.push_back(v[i]);
-      i++;
-    }
-    while (j <= r) {
-      aux.push_back(v[j]);
-      j++;
-    }
-  
-    for (int i = l; i <= r; i++) {
-      v[i] = aux[i - l];
-    }
-  }
+void merge(std::vector<int> &v, int l, int mid, int r);
