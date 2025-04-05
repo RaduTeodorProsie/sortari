155c5d865d73399995b014fa1e351b7782de0e39
Turcu Andrei Cristian <andreiturcu2005.24@gmail.com>
2025-04-05 17:45:43 +0300

    only1

--- sorts/mergesort.cpp
+++ sorts/mergesort.cpp
@@ -2,16 +2,16 @@
 
 void merge(std::vector<int> &v, int l, int mid, int r);
 
-void apply_mergesort(std::vector<int> &v, int l, int r) {
+void mergesort(std::vector<int> &v, int l, int r) {
   if (l == r)
     return;
 
   int mid = (l + r) / 2;
 
-  apply_mergesort(v, l, mid);
-  apply_mergesort(v, mid + 1, r);
+  mergesort(v, l, mid);
+  mergesort(v, mid + 1, r);
 
   merge(v, l, mid, r);
 }
 
-void mergesort(std::vector<int> &v) { apply_mergesort(v, 0, v.size() - 1); }
+void mergesort(std::vector<int> &v) { mergesort(v, 0, v.size() - 1); }
