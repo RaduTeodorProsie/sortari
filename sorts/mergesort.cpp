c514c83063cd68b0626f60e8cf67a71f9a81a3b5
Turcu Andrei Cristian <andreiturcu2005.24@gmail.com>
2025-04-05 17:44:59 +0300

    only onne this time

--- sorts/mergesort.cpp
+++ sorts/mergesort.cpp
@@ -1,31 +1,6 @@
 #include "sortari.h"
 
-void merge(std::vector<int> &v, int l, int mid, int r) {
-  std::vector<int> aux;
-  int i = l, j = mid + 1;
-
-  while (i <= mid and j <= r) {
-    if (v[i] < v[j]) {
-      aux.push_back(v[i]);
-      i++;
-    } else {
-      aux.push_back(v[j]);
-      j++;
-    }
-  }
-  while (i <= mid) {
-    aux.push_back(v[i]);
-    i++;
-  }
-  while (j <= r) {
-    aux.push_back(v[j]);
-    j++;
-  }
-
-  for (int i = l; i <= r; i++) {
-    v[i] = aux[i - l];
-  }
-}
+void merge(std::vector<int> &v, int l, int mid, int r);
 
 void apply_mergesort(std::vector<int> &v, int l, int r) {
   if (l == r)
--- sorts/timsort.cpp
+++ sorts/timsort.cpp
@@ -9,32 +9,7 @@ int calculate_minrun(int size) {
   return size + r;
 }
 
-void merge(std::vector<int> &v, int l, int mid, int r) {
-  std::vector<int> aux;
-  int i = l, j = mid + 1;
-
-  while (i <= mid and j <= r) {
-    if (v[i] < v[j]) {
-      aux.push_back(v[i]);
-      i++;
-    } else {
-      aux.push_back(v[j]);
-      j++;
-    }
-  }
-  while (i <= mid) {
-    aux.push_back(v[i]);
-    i++;
-  }
-  while (j <= r) {
-    aux.push_back(v[j]);
-    j++;
-  }
-
-  for (int i = l; i <= r; i++) {
-    v[i] = aux[i - l];
-  }
-}
+void merge(std::vector<int> &v, int l, int mid, int r);
 
 void insertion_sort(std::vector<int> &v, int l, int r) {
   for (int i = l + 1; i <= r; i++) {
